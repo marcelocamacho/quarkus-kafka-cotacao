3e949b004a68246009f3f6011c9396ecc38d370f
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2024-07-22 08:24:52 -0300

    Adiciona o método schedule ao Repository

--- src/main/java/org/br/mineradora/repository/QuotationRepository.java
+++ src/main/java/org/br/mineradora/repository/QuotationRepository.java
@@ -1,13 +1,21 @@
 package org.br.mineradora.repository;
 
 import org.br.mineradora.entity.QuotationEntity;
+import org.br.mineradora.service.QuotationService;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 
 // Quando o Quarkus fizer o start da aplicação, indica para o runtime que a classe é gerenciada pelo contexto do framework, ou seja ela é gerenciada pelo próprio quarkus
 @ApplicationScoped 
 public class QuotationRepository implements PanacheRepository<QuotationEntity> {
     
+    @Inject
+    QuotationService quotationService;
+
+    void schedule(){
+        quotationService.getCurrencyPrice();
+    }
 }
