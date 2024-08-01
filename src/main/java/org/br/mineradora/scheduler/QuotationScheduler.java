5ebd11e1a516ecd23fcf90933682d63aa76cf786
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2024-08-01 14:16:53 -0300

    Corrige o problema de NullPointerException no método currencyPriceInfo.getUSDBRL

--- src/main/java/org/br/mineradora/scheduler/QuotationScheduler.java
+++ src/main/java/org/br/mineradora/scheduler/QuotationScheduler.java
@@ -1,23 +1,27 @@
 package org.br.mineradora.scheduler;
 
-import org.br.mineradora.service.QuotationService;
-
 import io.quarkus.scheduler.Scheduled;
+import org.br.mineradora.service.QuotationService;
+import org.br.mineradora.message.KafkaEvents;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class QuotationScheduler {
+
+    private final Logger LOG = LoggerFactory.getLogger(QuotationScheduler.class);
 
     @Inject
     QuotationService quotationService;
 
     @Transactional
-    @Scheduled(every = "35s", identify)
+    @Scheduled(every = "35s", identity = "task-job")
     void schedule(){
+        LOG.info("-- Executando scheduler --");
         quotationService.getCurrencyPrice();
-    }
-    
-    
+    }  
 }
